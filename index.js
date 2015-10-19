5b5e95cdaea558cd2c8c96282503ea5de7757351
Bobby Powers <bobbypowers@gmail.com>
2015-10-19 09:31:10 -0400

    build: add LICENSE & update package.json

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+// Copyright 2015 Bobby Powers. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
 var pipe2 = require('bindings')('pipe2').pipe2;
 module.exports = pipe2;
